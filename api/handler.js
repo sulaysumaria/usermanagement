f38f1e751fd60a78c92cd386a564e762df1c9d38
Sulay Sumaria <sulaysumaria@gmail.com>
2019-04-07 11:59:40 +0530

    Added comments.

--- api/handler.js
+++ api/handler.js
@@ -2,6 +2,7 @@ const Joi = require('joi');
 
 const { db } = require('./helpers/db');
 
+// schema for validating user object when adding new user
 const userSchema = Joi.object().keys({
   firstName: Joi.string()
     .min(5)
@@ -16,6 +17,7 @@ const userSchema = Joi.object().keys({
     .required(),
 });
 
+// dummy route to check status of service
 async function ping() {
   return new Promise(async (resolve) => {
     try {
@@ -31,19 +33,26 @@ async function ping() {
   });
 }
 
+// get all users
 async function getUsers() {
   return new Promise(async (resolve) => {
     try {
+      // get connection to mongodb
       const dbConn = await db();
+
+      // get database instance
       const dbo = dbConn.db('usermanagement');
 
+      // get all users
       const users = await dbo
         .collection('users')
         .find()
         .toArray();
 
+      // close db connection
       await dbConn.close();
 
+      // return users
       return resolve({
         statusCode: 200,
         body: JSON.stringify({
@@ -66,6 +75,7 @@ async function getUsers() {
 async function createUser(event) {
   return new Promise(async (resolve) => {
     try {
+      // parse body
       let body;
       try {
         body = JSON.parse(event.body);
@@ -73,8 +83,10 @@ async function createUser(event) {
         body = {};
       }
 
+      // validate body
       const result = Joi.validate(body, userSchema, { abortEarly: false });
 
+      // return errors if any error
       if (result.error) {
         return resolve({
           statusCode: 400,
@@ -84,13 +96,19 @@ async function createUser(event) {
         });
       }
 
+      // get connection to mongodb
       const dbConn = await db();
+
+      // get database instance
       const dbo = dbConn.db('usermanagement');
 
+      // insert new user
       await dbo.collection('users').insert(body);
 
+      // close connection
       await dbConn.close();
 
+      // return response
       return resolve({
         statusCode: 200,
         body: JSON.stringify({
